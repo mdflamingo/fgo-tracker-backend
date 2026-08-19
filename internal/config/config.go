dc813a4f0ad6dd30693a563092d3f42c86e7cde4
Anastasiya <aiskoskina@yandex.ru>
2026-08-19 11:48:27 +0300

    add config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,36 @@
 package config
+
+import (
+	"flag"
+	"os"
+	"strings"
+)
+
+type Config struct {
+	RunAddr     string
+	LogLevel    string
+	DataBaseDSN string
+}
+
+func ParseFlags() *Config {
+	cfg := &Config{}
+
+	RunAddr := flag.String("a", ":8080", "address and port to run server")
+	logLevel := flag.String("l", "INFO", "log level")
+	dataBaseDSN := flag.String("d", "", "connect to postgres")
+
+	flag.Parse()
+
+	cfg.RunAddr = getEnvOrDefault("RUN_ADDRESS", *RunAddr)
+	cfg.LogLevel = strings.ToUpper(getEnvOrDefault("LOG_LEVEL", *logLevel))
+	cfg.DataBaseDSN = getEnvOrDefault("DATABASE_DSN", *dataBaseDSN)
+
+	return cfg
+}
+
+func getEnvOrDefault(envName, defaultValue string) string {
+	if envValue := os.Getenv(envName); envValue != "" {
+		return envValue
+	}
+	return defaultValue
+}
